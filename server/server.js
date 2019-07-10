61fa5c7ceaf4e09b6a3d7ed82b6626b1957c3217
Sithiz <Joshklaas1@gmail.com>
2019-07-09 23:28:46 -0500

    Working End point for users and posts

--- server/server.js
+++ server/server.js
@@ -1,12 +1,33 @@
-let express = require('express')
-let app = express()
-let PORT = 9000
+const express = require('express')
+const app = express()
+const PORT = 9000
+const { Client } = require('pg')
+const connectionString = 'postgres://sithis@localhost:5432/BlogWebsite';
 
 
-app.get()
+const client = new Client({
+	connectionString:connectionString
+})
 
+client.connect()
 
+app.use(express.urlencoded({extended: true})); 
+app.use(express.json());
+
+app.get('/Users', (request,response)=>{
+			client.query('SELECT * FROM Users',(error,result)=>{
+				if(error) throw error
+					response.send(result.rows[0])
+				})			
+})
+app.get('/Posts', (request,response)=>{
+			client.query('SELECT Username, Post_body FROM Posts', (error,result)=>{
+				if(error) throw error
+					response.send(result.rows)
+					console.log(result)
+			})		
+})	
 
 app.listen(PORT, ()=>{
-	console.log(`Im listening on port ${PORT}`)
-} )
+	console.log('connected via port:' + PORT)
+})
