90e3e3247ba0a7562ecd9841dbc8e7a785457731
Sithiz <Joshklaas1@gmail.com>
2019-07-10 12:12:25 -0500

    First Post Request Successful

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const connectionString = 'postgres://sithis@localhost:5432/BlogWebsite';
 const client = new Client({
 	connectionString:connectionString
 })
+const errorHandling = (error) => {
+	if(error) return error.stack
+}
 
 client.connect()
 
@@ -16,18 +19,51 @@ app.use(express.json());
 
 app.get('/Users', (request,response)=>{
 			client.query('SELECT * FROM Users',(error,result)=>{
-				if(error) throw error
-					response.send(result.rows[0])
+				errorHandling(error)
+					response.send(result.rows)
 				})			
+})
+app.get('/Users/:email', (request,response)=>{
+			let email = request.params.email
+			client.query('SELECT * FROM Users WHERE email = $1', [email], (error,result)=>{
+				errorHandling(error)
+					response.send(result.rows)
+			})
 })
 app.get('/Posts', (request,response)=>{
 			client.query('SELECT Username, Post_body FROM Posts', (error,result)=>{
-				if(error) throw error
+				errorHandling(error)
 					response.send(result.rows)
-					console.log(result)
 			})		
 })	
+app.get('/Posts/:postUserName',(request,response)=>{
+			let postUserName = request.params.postUserName
+			client.query('SELECT * FROM Posts WHERE Username = $1', [postUserName], (error,result)=>{
+				errorHandling(error)
+					response.send(result.rows)
+			})
+})
+app.post('/NewUser',(request,response)=>{
+			let data = {
+				username:request.body.username,
+				email:request.body.email
+			}
+			client.query('INSERT INTO Users(username,email) VALUES ($1,$2)', [data.username,data.email], (error,result)=>{
+				errorHandling(error)
+				response.send('success')
+			})
+			
+})
+
+
+
+
+
+
 
 app.listen(PORT, ()=>{
 	console.log('connected via port:' + PORT)
 })
+
+
+
